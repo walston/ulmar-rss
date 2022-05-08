9f03ae8f3b499e44e8d17d3517097376fb8cfd8d
Nathan Walston <walston@users.noreply.github.com>
2022-05-08 14:44:22 -0700

    Convert from podcast to xmlbuilder for more granular control

--- index.js
+++ index.js
@@ -1,56 +1,97 @@
-const fs = require("fs/promises");
+const {
+  statSync: stat,
+  writeFileSync: write,
+  copyFileSync: copy,
+} = require("fs");
 const path = require("path");
-const { Podcast } = require("podcast");
+const { create } = require("xmlbuilder2");
 
-const podcast = require("./info/podcast.json");
+const podcast_info = require("./info/podcast.json");
 const tracks = require("./info/tracks.json");
 
-const baseUrl = process.env.BASE_URL;
+const PRETTY = { prettyPrint: true };
 
+const baseUrl = process.env.BASE_URL;
 if (!baseUrl) {
   console.error("Missing BASE_URL");
   process.exit(128);
 }
 
-for (const key in podcast) {
-  if ("string" === typeof podcast[key]) {
-    podcast[key] = podcast[key].replace("$baseUrl", baseUrl);
-  }
-  podcast.itunesImage = podcast.imageUrl;
+/** @param {string} input */
+function replaceInString(input) {
+  return input.replaceAll("$baseUrl", baseUrl);
 }
 
-const feed = new Podcast(podcast);
-
-for (const trackName in tracks) {
-  const track = tracks[trackName];
-  for (const key in track) {
-    if ("string" === typeof track[key]) {
-      track[key] = track[key].replace("$baseUrl", baseUrl);
-    }
-  }
-
-  track.enclosure = {
-    url: track.url,
-    file: path.resolve("audio", trackName),
-  };
-
-  feed.addItem(track);
+/** @param {Date | string} date */
+function formatDate(date = new Date()) {
+  return date instanceof Date
+    ? date.toUTCString()
+    : new Date(date).toUTCString();
 }
 
-output(feed.buildXml())
-  .then(() => process.exit())
-  .catch((err) => {
-    console.error(err);
-    process.exit(err.errno || 1);
+/** @param {object} obj */
+function populateEnvVariables(obj) {
+  for (const key in obj) {
+    if (typeof obj[key] === "string") obj[key] = replaceInString(obj[key]);
+    else if (typeof obj[key] === "object") populateEnvVariables(obj[key]);
+  }
+}
+
+populateEnvVariables(podcast_info);
+populateEnvVariables(tracks);
+
+try {
+  const feed = create({
+    rss: {
+      "@version": "2.0",
+      "@xmlns:itunes": "http://www.itunes.com/dtds/podcast-1.0.dtd",
+      "@xmlns:atom": "http://www.w3.org/2005/Atom",
+      "@xmlns:googleplay": "http://www.google.com/schemas/play-podcasts/1.0",
+      channel: {
+        title: podcast_info.title,
+        link: podcast_info.url,
+        description: podcast_info.description,
+        "atom:link": {
+          "@href": podcast_info.homepage,
+          "@rel": "self",
+          "@type": "application/rss+xml",
+        },
+        "itunes:owner": {
+          "itunes:name": podcast_info.author,
+          "itunes:email": podcast_info.authorEmail,
+        },
+        "itunes:author": podcast_info,
+        "itunes:image": {
+          "@href": podcast_info.imageUrl,
+        },
+        "googleplay:block": "yes",
+        "itunes:block": "Yes",
+        language: "en-US",
+        pubDate: formatDate(),
+        lastBuildDate: formatDate(),
+        item: Object.entries(tracks).map(([guid, track]) => ({
+          title: track.title,
+          description: track.description,
+          link: track.url,
+          pubDate: formatDate(track.date),
+          guid: { "@isPermalink": "false", "#": guid },
+          enclosure: {
+            "@url": track.url,
+            "@length": stat(track.file).size,
+            "@type": "audio/mp4",
+          },
+        })),
+      },
+    },
   });
 
-async function output(xml) {
   const inDir = path.resolve(".", "src");
   const outDir = path.resolve(".", "dist");
 
-  await fs.writeFile(path.resolve(outDir, "rss.xml"), xml);
-  await fs.copyFile(
-    path.resolve(inDir, "cover.jpg"),
-    path.resolve(outDir, "cover.jpg")
-  );
+  write(path.resolve(outDir, "rss.xml"), feed.toString(PRETTY));
+  copy(path.resolve(inDir, "cover.jpg"), path.resolve(outDir, "cover.jpg"));
+  console.log(feed.toString(PRETTY));
+} catch (err) {
+  console.error(err);
+  process.exit(err.errno || 1);
 }
